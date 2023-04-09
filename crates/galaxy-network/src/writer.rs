c50d0664e4a46feb81c12f9cb345b99fa3596ee3
Nero <nerodono0@gmail.com>
2023-04-10 02:17:59 +0300

    Rewrite vectored write function to be more describable

--- crates/galaxy-network/src/writer.rs
+++ crates/galaxy-network/src/writer.rs
@@ -242,9 +242,9 @@ impl<W: Write, C> GalaxyWriter<W, C> {
         mut prepend: &[u8],
         append: &[u8],
     ) -> io::Result<()> {
-        let plen = prepend.len();
-        let alen = append.len();
-        let total = alen + plen;
+        let prepend_len = prepend.len();
+        let append_len = append.len();
+        let total = append_len + prepend_len;
 
         if !self.raw.is_write_vectored() {
             // Since write is not vectored we'll just create
@@ -256,27 +256,33 @@ impl<W: Write, C> GalaxyWriter<W, C> {
             return self.raw.write_all(&vec).await;
         }
 
-        let mut written = 0_usize;
         let mut ios = [IoSlice::new(prepend), IoSlice::new(append)];
-        while written < total {
-            let cur_wrote = self.raw.write_vectored(&ios).await?;
-            written += cur_wrote;
+        let mut sent = 0_usize;
+        loop {
+            let cur_wrote @ 1.. = self.raw.write_vectored(&ios).await? else {
+                break Err(io::Error::last_os_error());
+            };
+            sent += cur_wrote;
 
-            if written != total {
-                if written >= plen {
-                    let append_offset = total - written;
-                    return self
-                        .raw
-                        .write_all(&append[append_offset..])
-                        .await;
-                }
-
-                prepend = &prepend[cur_wrote..];
-                ios[0] = IoSlice::new(prepend);
+            if sent >= total {
+                break Ok(());
             }
-        }
+            // sent < total
+            if sent >= prepend_len {
+                // then prepend buffer was fully wrote
+                // we write the remaining into the single system call
+                // (ideally)
+                return self
+                    .raw
+                    .write_all(&append[(sent - prepend_len)..])
+                    .await;
+            }
 
-        Ok(())
+            // sent < prepend_len
+            // then we should remove written to stream bytes
+            prepend = &prepend[cur_wrote..];
+            ios[0] = IoSlice::new(prepend);
+        }
     }
 }
 
